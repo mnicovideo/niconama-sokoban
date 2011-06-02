9f0d877ae90105cc068bdc3bb0626d1f0975fe22
m_at_nicovideo <m_at_nicovideo@live.com>
2011-06-02 01:32:28 +0000

    マップデータを外部ファイルから都度呼び出せるようにした

--- game/js/field.js
+++ game/js/field.js
@@ -10,17 +10,39 @@
     /**
      * レベル毎の画面表示内容
      */
-    Field.map = [];
+    Field.Map = {};
+
+    /**
+     * マップデータを定義ファイルから取得
+     **/
+    Field.Map.get = function(level) {
+        /**
+         * 定義ファイルの読み込み
+         **/
+        $.ajax({
+            'url'       : 'map/level'+level+'.json',
+            'async'     : false,
+            'dataType'  : 'json',
+            'success'   : function(json){Field.Map.data = json.data;}
+        });
+    };
 
     /**
      * ゲーム画面の作成
      */
-    Field.create = function(){
+    Field.create = function(level){
         var i, ii, mapElement, tr, s;
+
+        //ゲーム画面のhtml要素。骨格。
         mapElement = $('<table>',{'class':'sokoban'}).append('<tbody>');
-        for(i=0; i<Field.map[Game.level].length; i++){
+
+        //マップデータの取得
+        Field.Map.get(level);
+
+        //マップデータを元にゲーム画面要素を生成
+        for(i=0; i<Field.Map.data.length; i++){
             tr = $('<tr>');
-            s = String(Field.map[Game.level][i][0]);
+            s = Field.Map.data[i];
             for(ii=0; ii<s.length; ii++)
                 switch(s.substr(ii,1)){
                     case ' ' : tr.append( $('<td>',{'class':'empty'}) ); break;
--- game/test/m/testfieldmap.js
+++ game/test/m/testfieldmap.js
@@ -1,5 +1,6 @@
 //ゲーム画面関係
 (function(global){
+/*
     var map = global.Sokoban.Field.map;
 
     //Lv 0:テストマップ
@@ -7,5 +8,5 @@
         ['  '],
         ['  ']
     ]);
-
+*/
 })(this);
