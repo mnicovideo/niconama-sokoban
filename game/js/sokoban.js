09fbc688931ee799bc643b45b59e89e35d07b0cb
m_at_nicovideo <m_at_nicovideo@live.com>
2011-06-02 01:31:27 +0000

    メソッドチェーンに対応

--- game/js/sokoban.js
+++ game/js/sokoban.js
@@ -1,10 +1,23 @@
 //ゲームの本体
 (function(global){
+
     /**
      * ゲームの本体オブジェクト
      */
-    var Sokoban = global.Sokoban = function(e){
-        $(e).append(Sokoban.Field.create());
+    var Sokoban = global.Sokoban = function(element, level){
+
+        //レベル設定
+        this.level = level || this.level || 0;
+
+        //ゲームスタート
+        this.start = function(){
+            $(element).empty().append(Sokoban.Field.create(this.level));
+            return this;
+        };
+
+        //メソッドチェーン
+        return this;
+
     };
-    Sokoban.level = 0;
+
 })(this);
